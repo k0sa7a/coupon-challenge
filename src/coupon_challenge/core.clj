3d81cc6442b4f70758bbb49aa733ff3eb093f936
Stefan Filev <stefan.s.filev@gmail.com>
2023-11-26 16:51:38 +0000

    introduction of calculatePrice function

--- src/coupon_challenge/core.clj
+++ src/coupon_challenge/core.clj
@@ -6,23 +6,48 @@
 (def ferrariCar (struct car "ferrari" 100000))
 (def fiat (struct car "fiat" 20000))
 
+;(println  (:CarPrice bmwCar)  )
+
 (def validCoupons [1234 9000 7777])
 
 (defn checkCoupon
   [coupon]
-  (contains? (set validCoupons)  coupon))
+  (contains? (set validCoupons) coupon))
 
-(defn returnOptimalCar
+
+(defn calculatePrice
+  [budget valid]
+
+  (if valid
+    (do (println "valid 1")
+        (println "valid 2")
+        "VALID")
+
+    (do (println "invalid 1")
+        (println "invalid 2")
+        "INVALID")
+
+    )
+
+  )
+
+
+
+
+(defn -main
   [budget coupon]
 
   (def checkBool (checkCoupon coupon))
-  (println checkBool)
+  (def result (calculatePrice budget checkBool) )
+  (println result)
+
   )
 
-(println "Valid Coupon; budget 50000 should return BMW 48k and FIAT 16k")
 
-(returnOptimalCar 50000 1234)
+
+(println "Valid Coupon; budget 50000 should return BMW 48k and FIAT 16k")
+(-main 50000 1234)
 
 
 (println "Invalid Coupon; budget 50000 should return invalid coupon and FIAT 20k")
-(returnOptimalCar 50000 9999)
+(-main 50000 9999)
