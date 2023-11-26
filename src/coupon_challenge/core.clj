9eb6fee5b82d106ddc296531ac5fc6eb88ceae27
Stefan Filev <stefan.s.filev@gmail.com>
2023-11-26 16:32:23 +0000

    Helper function for checking the coupon

--- src/coupon_challenge/core.clj
+++ src/coupon_challenge/core.clj
@@ -3,9 +3,26 @@
 (defstruct car :CarBrand :CarPrice)
 
 (def bmwCar (struct car "bmw" 60000))
+(def ferrariCar (struct car "ferrari" 100000))
+(def fiat (struct car "fiat" 20000))
 
-(defn foo
+(def validCoupons [1234 9000 7777])
 
-  []
-  (println bmwCar))
-(foo)
+(defn checkCoupon
+  [coupon]
+  (contains? (set validCoupons)  coupon))
+
+(defn returnOptimalCar
+  [budget coupon]
+
+  (def checkBool (checkCoupon coupon))
+  (println checkBool)
+  )
+
+(println "Valid Coupon; budget 50000 should return BMW 48k and FIAT 16k")
+
+(returnOptimalCar 50000 1234)
+
+
+(println "Invalid Coupon; budget 50000 should return invalid coupon and FIAT 20k")
+(returnOptimalCar 50000 9999)
